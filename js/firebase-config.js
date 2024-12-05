d331a337b5385c68e5a116c57ffc8f93f0e1fb6a
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-05 15:24:40 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,16 +1,20 @@
+// Importa las funciones necesarias de Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
 
 // Configuración de Firebase
 const firebaseConfig = {
-   apiKey: "AIzaSyDhzxutPaa1OxXDe2d4T5pNxGkNsrdlifs",
-    authDomain: "database-02549z.firebaseapp.com",
-    databaseURL: "https://database-02549z-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "database-02549z",
-    storageBucket: "database-02549z.firebasestorage.app",
-    messagingSenderId: "970508003334",
-    appId: "1:970508003334:web:bf44e95b6c34b369d7bb87",
-    measurementId: "G-VW9QXZQ6CW"
+  apiKey: "AIzaSyDhzxutPaa1OxXDe2d4T5pNxGkNsrdlifs",
+  authDomain: "database-02549z.firebaseapp.com",
+  databaseURL: "https://database-02549z-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "database-02549z",
+  storageBucket: "database-02549z.appspot.com",
+  messagingSenderId: "970508003334",
+  appId: "1:970508003334:web:bf44e95b6c34b369d7bb87",
+  measurementId: "G-VW9QXZQ6CW"
 };
 
 // Inicializa Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
+const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
+
