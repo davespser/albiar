7d9e2f615a6efdde987931b527af1f1c4e4b6f7f
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-05 14:49:40 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,13 +2,14 @@
 
 // Configuración de Firebase
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://TU_PROJECT_ID.firebaseio.com",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_PROJECT_ID.appspot.com",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+   apiKey: "AIzaSyDhzxutPaa1OxXDe2d4T5pNxGkNsrdlifs",
+    authDomain: "database-02549z.firebaseapp.com",
+    databaseURL: "https://database-02549z-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "database-02549z",
+    storageBucket: "database-02549z.firebasestorage.app",
+    messagingSenderId: "970508003334",
+    appId: "1:970508003334:web:bf44e95b6c34b369d7bb87",
+    measurementId: "G-VW9QXZQ6CW"
 };
 
 // Inicializa Firebase
