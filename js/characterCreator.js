5a15a02cfcdf74539642f3669b15e6ef9ecab4a4
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-07 13:39:23 +0100

    Update characterCreator.js

--- js/characterCreator.js
+++ js/characterCreator.js
@@ -35,6 +35,7 @@ const questions = [
 // Elementos del DOM
 const container = document.createElement("div");
 container.id = "questionnaire";
+container.style.width = "15px";
 container.style.position = "absolute";
 container.style.top = "1%";
 container.style.right = "20px"; // Cambia 'left' a 'right' para posicionarlo a la derecha
