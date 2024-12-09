242802d84593699aa8cdf18fa6a2c2b985d797e4
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-09 16:26:22 +0100

    Update ground.js

--- js/ground.js
+++ js/ground.js
@@ -1,1 +1,19 @@
 
+// ground.js
+import * as THREE from "three";
+
+export function createFloor() {
+  const floorTexture = new THREE.TextureLoader().load("./js/grasslight-big.png");
+  floorTexture.wrapS = THREE.RepeatWrapping;
+  floorTexture.wrapT = THREE.RepeatWrapping;
+  floorTexture.repeat.set(10, 10);
+
+  const floorMaterial = new THREE.MeshStandardMaterial({ map: floorTexture });
+  const floorGeometry = new THREE.PlaneGeometry(50, 50);
+
+  const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+  floor.rotation.x = -Math.PI / 2;
+  floor.receiveShadow = true;
+
+  return floor;
+}
