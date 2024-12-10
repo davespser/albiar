bc3f2a47e0f969ea477bcf1fda064b57f32ad61d
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-10 08:36:09 +0100

    Update ground.js

--- js/ground.js
+++ js/ground.js
@@ -3,8 +3,8 @@ import * as THREE from "three";
 export function createFloor() {
   // Cargar el mapa de desplazamiento y la textura del terreno
   const textureLoader = new THREE.TextureLoader();
-  const displacementMap = textureLoader.load("./textures/heightmap.jpg"); // Ruta del mapa de desplazamiento
-  const terrainTexture = textureLoader.load("./textures/terrain1.jpg"); // Ruta de la textura del terreno
+  const displacementMap = textureLoader.load("./js/textures/heightmap.jpg"); // Ruta del mapa de desplazamiento
+  const terrainTexture = textureLoader.load("./js/textures/terrain1.jpg"); // Ruta de la textura del terreno
 
   // Configurar la textura del terreno
   terrainTexture.wrapS = THREE.RepeatWrapping;
