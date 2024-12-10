ec4238d975c5b098530fd137f82e3ab824676749
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-10 11:52:50 +0100

    Create TerrainMesh.js

---
+++ js/terrain2/TerrainMesh.js
@@ -0,0 +1,15 @@
+import * as THREE from 'three';
+import { TerrainGeometry } from './TerrainGeometry';
+
+export class TerrainMesh {
+  static create(vertices, indices) {
+    const geometry = TerrainGeometry.create(vertices, indices);
+    const material = TerrainGeometry.createMaterial();
+    
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+    
+    return mesh;
+  }
+}
