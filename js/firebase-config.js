b434c343a6e2b0b3a5c45abe7240c9f944cbbbfc
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-11 10:07:18 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,5 +1,23 @@
-import { auth, database } from "./firebase-config.js";
-import { ref, update } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
 
-const userRef = ref(database, "users/123");
-await update(userRef, { name: "John Doe" });
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDhzxutPaa1OxXDe2d4T5pNxGkNsrdlifs",
+  authDomain: "database-02549z.firebaseapp.com",
+  databaseURL: "https://database-02549z-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "database-02549z",
+  storageBucket: "database-02549z.appspot.com",
+  messagingSenderId: "970508003334",
+  appId: "1:970508003334:web:bf44e95b6c34b369d7bb87",
+  measurementId: "G-VW9QXZQ6CW",
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app); // Inicializar la base de datos
+
+// Exportar servicios
+export { auth, database };
